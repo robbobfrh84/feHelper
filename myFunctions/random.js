573ea04d9b7752621832d17f250828f4b187492e
Bob Main <bobmain49@gmail.com>
2018-02-28 15:34:12 -0600

    breaking code out into individual files

---
+++ myFunctions/random.js
@@ -0,0 +1,13 @@
+// get random max min.
+function random(min, max) {
+  return Math.floor(Math.random() * (max - min + 1) + min);
+}
+x = random(20,40)
+
+//!!! used with random(). Returns 3 0-to-255 random numbers to be used for random color.
+function rgbR(){
+  return 'rgb('+random(0,255)+', '+random(0,255)+', '+random(0,255)+')';
+}
+
+//Returns the base-16 equivalent 0-16777215 in base 10.
+'#'+Math.floor(Math.random()*16777215).toString(16);
